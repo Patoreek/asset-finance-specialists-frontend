42e03bcd052eb285cea8e0c4ce6c36a4b3596987
Patrick Minda <patrick.minda@hotmail.com>
2024-12-17 15:20:59 +1100

    tailwind + shadcn integrated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Button } from "@/components/ui/button";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>Deploy to AWS Amplify.</div>
-    </>
+    <div className="bg-gray-50">
+      <div className="flex justify-center items-center h-screen">
+        <Button>Shadcn/ui integrated.</Button>
+      </div>
+    </div>
   );
 }
 
